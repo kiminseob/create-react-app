ccd26e825b89d5f6df9567e079d93295cc41311e
kiminseob <kis6473@naver.com>
2022-03-18 00:18:29 +0900

    fix: component import 경로 수정

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  font-weight: 700;
+  font-size: 2rem;
+`;
+
+function MyAppHome() {
+  return <Header>my app home</Header>;
+}
+
+export default MyAppHome;
